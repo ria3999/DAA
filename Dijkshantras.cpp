0fe97459132ac942e7632435031320ee0708934f
RIA GOEL <riagoel3999@gmail.com>
2021-09-19 21:09:39 +0530

    Update Dijkshantras.cpp

--- Dijkshantras.cpp
+++ Dijkshantras.cpp
@@ -1,66 +1,66 @@
-#include<iostream>
-#include<list>
+#include <iostream>
+#include <list>
 #include<vector>
 #include<set>
+#include <climits>
 using namespace std;
 class Graph
 {
     int V;
-    list<int>*l;
+     list<pair<int,int> >*l;
     public:
-        Graph(int v)
+    Graph(int V)
+    {
+        this->V=V;
+        l=new list<pair<int,int> >[V];
+        
+    }
+    void addEdge(int s,int d,int w)
+    {
+        l[s].push_back(make_pair(d,w));
+        l[d].push_back(make_pair(s,w));
+    }
+    int dijakstras(int src,int des)
+    {
+        vector<int>dist(V,INT_MAX);
+        set<pair<int,int> >s;
+        dist[src]=0;
+        s.insert({src,0});
+        while(!s.empty())
         {
-            V=v;
-            l=new list<int>[V];
-        }
-        void addEdge(int i,int j,bool undir=true)
-        {
-            l[i].push_back(j);
-            if(undir)
-                l[j].push_back(i);
-        }
-        int dijkshatra(int src,int dest)
-        {
-            vector<int>dist(V,INT_MAX);
-            set<pair<int,int> >s;
-            dist[src]=0;
-            s.insert({0,src});
-            while(!s.empty())
+            auto it=s.begin();
+            int node=it->first;
+            int distanceTillNow=it->second;
+            s.erase(it);
+            for(auto nbrPair: l[node])
             {
-                auto it=s.begin();
-                int node=it->second;
-                int distanceTillNow=it->first;
-                s.erase(it);
-                for(auto nbrpair:l[node])
+                int nbr=nbrPair.first;
+                int currentEdge=nbrPair.second;
+                if(distanceTillNow+currentEdge<dist[nbr])
                 {
-                    int nbr=nbrpair.second;
-                    int currentEdge=nbrpair.first;
-                    if(distaceTillNow+currentEdge<dist[nbr])
-                    {
-                        auto f=s.find({dist[nbr],nbr});
-                        if(f!=s.end())
-                        {
-                            s.erase(f);
-                        }
-                        distance[nbr]=distanceTillNow+currentEdge;
-                        s.insert({dist[nbr],nbr});
-                    }
+                    auto f=s.find({nbr,dist[nbr]});
+                if(f!=s.end())
+                s.erase(f);
+                    dist[nbr]=distanceTillNow+currentEdge;
+                    s.insert({nbr,dist[nbr]});
                 }
 
             }
-            return dist[dest];
         }
-
+        return dist[des];
+    }
 };
-int main()
+
+
+int main() 
 {
     Graph g(5);
     g.addEdge(0,1,1);
-    g.addEdge(1,2,2);
+    g.addEdge(1,2,1);
     g.addEdge(0,2,4);
     g.addEdge(0,3,7);
     g.addEdge(3,2,2);
     g.addEdge(3,4,3);
-    cout<<g.dijkshatra(0,4);
+    cout<<g.dijakstras(0,4)<<endl;
     return 0;
 }
